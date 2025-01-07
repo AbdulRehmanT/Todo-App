6a361dac7a28c511f76e3491df412aafbe7cdd12
Abdul Rehman <talhaabbas79@gmail.com>
2025-01-07 11:35:24 +0500

    vercel file created

--- todo-backend/index.mjs
+++ todo-backend/index.mjs
@@ -1,10 +1,13 @@
 import express from "express";
+
 const app = express();
 const port = 4000;
 
 const todo = [];
 
 app.use(express.json());
+app.use(cors( {origin: BASE_URL}))
+
 
 // Get All Todo Route
 app.get("/api/v1/todos", (req, res) => {
