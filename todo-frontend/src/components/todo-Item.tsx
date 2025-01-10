cc403f70ee78a4bbecaac21e9dbde37a09ef32ac
Abdul Rehman <talhaabbas79@gmail.com>
2025-01-10 19:22:42 +0500

    remove unused tags

--- todo-frontend/src/components/todo-Item.tsx
+++ todo-frontend/src/components/todo-Item.tsx
@@ -11,28 +11,25 @@ interface TodoItemProps {
   setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
 }
 
-
-const handleDelete = async (todo: Todo, setTodos: React.Dispatch<React.SetStateAction<Todo[]>>) => {
-  const url = process.env.NEXT_PUBLIC_BACK_BASE_URL;
-  try {
-
-    await axios.delete(`${url}/api/v1/todo/${todo.id}`);
-
-
-    setTodos((prevTodos) => prevTodos.filter((item) => item.id !== todo.id));
-  } catch (err) {
-    console.error("Error deleting todo:", err);
-  }
-};
-
 export default function TodoItem({ todo, setTodos }: TodoItemProps) {
-  const todoItem = todo.todoContent;
+  const url = process.env.NEXT_PUBLIC_BACK_BASE_URL;
+  
+  const handleDelete = async () => {
+    try {
+      await axios.delete(`${url}/api/v1/todo/${todo.id}`);
+
+      setTodos((prevTodos) => prevTodos.filter((item) => item.id !== todo.id));
+    } catch (err) {
+      console.error("Error deleting todo:", err);
+    }
+  };
+
   return (
     <div className="flex space-x-2 mt-4 items-center justify-between">
-      <span>{todoItem}</span>
+      <span>{todo.todoContent}</span>
       <div className="flex gap-2">
         <Button variant="outline">Edit</Button>
-        <Button onClick={() => handleDelete(todo, setTodos)}>Delete</Button> {/* Pass 'todo' and 'setTodos' */}
+        <Button onClick={handleDelete}>Delete</Button>
       </div>
     </div>
   );
